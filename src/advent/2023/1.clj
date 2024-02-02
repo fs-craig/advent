b55200c6886d4619f2946754a9c79578f8b5c3cc
craig <craig.flewelling@gmail.com>
2024-02-02 15:22:24 +0000

    Added overlapping regexes for question 1 part 2.

--- src/advent/2023/1.clj
+++ src/advent/2023/1.clj
@@ -5,6 +5,8 @@
 (def resources "resources/2023/1/")
 (def example-1 (str resources "example_1.txt"))
 (def part-1 (str resources "part_1.txt"))
+(def example-1-2 (str resources "example_1_2.txt"))
+
 (load-file (str resources "answers.clj"))
 (def digit-words
   {"one" 1
@@ -18,28 +20,32 @@
    "nine" 9})
 
 (def worded-regex
-  (re-pattern (apply str "\\d|" (interpose "|" (keys digit-words)))))
+  (re-pattern (apply str "(?=(\\d|" (conj (vec (interpose "|" (keys digit-words))) "))"))))
+(def words [worded-regex second])
 
 (def digit-regex #"\d")
+(def digits [digit-regex identity])
 
 (defn word->digit
   [v]
   (if (= (count v) 1)
     v
     (digit-words v)))
-      
-(defn calibration [regex s]
-  (let [digits (map word->digit (re-seq regex s))]
+
+(defn calibration [[regex compf] s]
+  (let [digits (map (comp word->digit compf) (re-seq regex s))]
     (read-string (str (first digits) (last digits)))))
 
-(defn sum-calibration [regex path]
+(defn sum-calibration [[regex compf :as ins] path]
   (->> (string/split (slurp path) #"\n")
-       (map (partial calibration regex))
+       (map (partial calibration ins))
        (reduce +)))
 
 (deftest check-answers
-  (is (= (sum-calibration digit-regex example-1) example-1-answer))
-  (is (= (sum-calibration digit-regex part-1) part-1-answer)))
+  (is (= (sum-calibration digits example-1) example-1-answer))
+  (is (= (sum-calibration digits part-1) part-1-answer))
+  (is (= (sum-calibration words example-1-2) example-1-2-answer)))
 
-;;end of day Tuesday
-;;worded-regex isn't working right.  what happens if 9 inside of nine?
+
+;;end of day friday
+;;tests pass.  cleanup the worded-regex and then try the problem.

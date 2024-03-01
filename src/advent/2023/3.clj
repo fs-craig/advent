175a15ada4cc18383247cfdecfcd5c6930dbb032
craig <craig.flewelling@gmail.com>
2024-03-01 21:06:11 +0000

    Setting up problem 3 i/o

--- src/advent/2023/3.clj
+++ src/advent/2023/3.clj
@@ -6,7 +6,19 @@
 (def example-1 (str resources "example_1.txt"))
 (load-file (str resources "answers.clj"))
 
-;;
+;;rewrite this for rows since this won't work with >10 items in first row
+(defn item-at [rows row col]
+ (nth items (+ row col)))
+
+;;pull the rows into get-rows
+;;use item-at here.
+(defn index-items [path]
+  (let [rows (->> (slurp path)
+                  (string/split-lines))]
+    (for [i (range (count rows))
+          j (range (count (first rows)))]
+      [i j (str (nth (nth rows i) j))])))
+
 (deftest check-answers
   (is (= example-1
          example-1-answer)))
